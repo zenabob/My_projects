1e97e717b4f3cc98f48703a225e1b77c4a7385e5
zenabob <91099209+zenabob@users.noreply.github.com>
2023-12-16 22:17:25 +0200

    Delete AcademicForum App directory

--- AcademicForum App/src/screens/index.js
+++
@@ -1,4 +0,0 @@
-export { default as StartScreen } from "./StartScreen";
-export { default as ContactUsScreen } from "./ContactusScreen";
-export { default as SearchScreen } from "./SearchScreen";
-export { default as PodcastScreen } from "./PodcastScreen";
